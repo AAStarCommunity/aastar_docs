855d542b1c6407b452a9cede43eb47b01e14f11b
dylanyang <yl976931569@163.com>
2024-07-03 18:49:28 +0800

    update

---
+++ docs/component/api-doc.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="redoc-container"></div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'RedocComponent',
+    mounted() {
+      const script = document.createElement('script');
+      script.src = "https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js";
+      script.onload = () => {
+        Redoc.init('/swagger.json', {}, document.getElementById('redoc-container'));
+      };
+      document.body.appendChild(script);
+    }
+  }
+  </script>
+  
+  <style scoped>
+  </style>
+  
